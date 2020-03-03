4784899cb2728d6e59ceaeed3964b9c4a8c90498
qiuzhanghua <qiuzhanghua@icloud.com>
2020-03-03 17:10:09 +0800

    learn mysql and r2d2 for mysql

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use r2d2;
+use r2d2_mysql;
+
+use r2d2_mysql::mysql::{Opts, OptsBuilder};
+use r2d2_mysql::MysqlConnectionManager;
+use std::sync::Arc;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Payment {
+    customer_id: i32,
+    amount: i32,
+    account_name: Option<String>,
+}
+
+fn main() {
+    dotenv::dotenv().ok();
+    // let db_url = "mysql://app:app@localhost:3306/app";
+    let db_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let opts = Opts::from_url(&db_url).unwrap();
+    let builder = OptsBuilder::from_opts(opts);
+    let manager = MysqlConnectionManager::new(builder);
+    let pool = Arc::new(r2d2::Pool::builder().max_size(4).build(manager).unwrap());
+    let mut conn = pool.clone().get().unwrap();
+    let x = conn.query(r#"select version() v"#);
+    println!("{:?}", x.unwrap().last());
+}
